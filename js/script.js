a6b675741af8b6ca8f2d6168528531d30a54d57b
Nick <niiickolay@gmail.com>
2018-08-29 20:41:14 +0300

    made contacts section

--- js/script.js
+++ js/script.js
@@ -7,4 +7,24 @@ menuButton.addEventListener("click", function(evt) {
   evt.preventDefault();
 
   menu.classList.toggle("main-nav--shown");
-})
+});
+
+
+function initMap() {
+  var point = {lat: 59.93873, lng: 30.32310};
+  var center = {lat: 59.9387, lng: 30.3230};
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 17,
+    center: center
+  });
+  var image = {
+    url: "img/icon-map-marker.svg",
+    size: new google.maps.Size(67, 100),
+    anchor: new google.maps.Point(33.5, 100)
+  }
+  var marker = new google.maps.Marker({
+    position: point,
+    map: map,
+    icon: image
+  });
+}
