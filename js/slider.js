f1f0f8b9787da10456afdf3a7a3ba01e54b23815
Nick <niiickolay@gmail.com>
2018-09-01 12:30:14 +0300

    added auto-moving to slider

--- js/slider.js
+++ js/slider.js
@@ -1,25 +1,41 @@
 function slider(argument) {
-  const viewport = document.querySelector(".slider__viewport");
-  const slidesFrame = document.querySelector(".slider__slides");
-  var slidesList = document.querySelectorAll(".slider__item");
+  const slider = document.querySelector(".slider");
+  const viewport = slider.querySelector(".slider__viewport");
+  const slidesFrame = slider.querySelector(".slider__slides");
+  var slidesList = slider.querySelectorAll(".slider__item");
   const slidesCount = slidesList.length;
-  const dotsContainer = document.querySelector(".slider__dots");
-  const buttonsContainer = document.querySelector(".slider__buttons");
+  const dotsContainer = slider.querySelector(".slider__dots");
+  const buttonPrev = slider.querySelector(".slider__prev");
+  const buttonNext = slider.querySelector(".slider__next");
 
-  let firstPosition = 0;
+  let firstPosition = argument.firstPosition || 0;
+
   let currentPosition = firstPosition;
-  let newPosition = firstPosition;
+  let newPosition = currentPosition;
+
+  // (function() {
+  //   let stepLength = viewport.offsetWidth;
+  //   let path = (0 - newPosition) * stepLength;
+  //   slidesFrame.style.transform = `translateX(${path}px)`;
+  // })();
+
 
   function moveSlides() {
     let stepLength = viewport.offsetWidth;
     let path = (firstPosition - newPosition) * stepLength;
     slidesFrame.style.transform = `translateX(${path}px)`;
-
-    console.log(stepLength);
-    console.log(path);
   }
 
-  function initControls(n) {
+  function autoMoving() {
+    let timerId = setTimeout(function() {
+      newPosition = currentPosition >= slidesCount - 1 ? 0 : newPosition + 1;
+      initButton();
+
+      timerId = setTimeout(autoMoving, 2000);
+    }, 2000);
+  }
+
+  function makeDots(n) {
     for (let i = 0; i < n; i++) {
       let dotButton = document.createElement("button");
       dotButton.classList.add("slider__dot");
@@ -31,62 +47,71 @@ function slider(argument) {
 
       dotButton.addEventListener("click", function() {
         newPosition = i;
-
-        setCurrentDot();
-        currentPosition = newPosition;
-
-        moveSlides();
+        initButton();
       });
 
       dotsContainer.appendChild(dotButton);
     }
   }
 
-  function initButtons() {
-    let buttonPrev = document.createElement("button");
-    buttonPrev.classList.add("slider__button", "slider__prev");
-
-    if (currentPosition <= 0) {
-      // buttonPrev.setAttribute("disabled", "disabled");
-    }
-
-    buttonPrev.addEventListener("click", function() {
-      newPosition--;
-      setCurrentDot();
-      currentPosition = newPosition;
-      moveSlides();
-    });
-
-
-
-    let buttonNext = document.createElement("button");
-    buttonNext.classList.add("slider__button", "slider__next");
-
-    if(currentPosition >= slidesCount) {
-      buttonNext.setAttribute("disabled", "disabled");
-    }
-
-    buttonNext.addEventListener("click", function() {
-      newPosition++;
-      setCurrentDot();
-      currentPosition = newPosition;
-      moveSlides();
-    });
-
-    buttonsContainer.appendChild(buttonPrev);
-    buttonsContainer.appendChild(buttonNext);
-  }
 
   function setCurrentDot() {
-    let dotsList = document.querySelectorAll(".slider__dot");
+    if (argument.sliderDots == false) {
+      return;
+    }
+
+    let dotsList = slider.querySelectorAll(".slider__dot");
     dotsList[currentPosition].classList.remove("slider__dot--current");
     dotsList[newPosition].classList.add("slider__dot--current");
   }
 
+  function checkDisabled() {
+    if (newPosition === 0) {
+      buttonPrev.setAttribute("disabled", "disabled");
+    } else if (currentPosition === 0 && newPosition !== 0) {
+      buttonPrev.removeAttribute("disabled", "disabled");
+    }
 
-  initControls(slidesCount);
-  initButtons();
+    if (newPosition === slidesCount - 1) {
+      buttonNext.setAttribute("disabled", "disabled");
+    } else if (currentPosition === slidesCount - 1 && newPosition !== slidesCount - 1) {
+      buttonNext.removeAttribute("disabled", "disabled");
+    }
+  }
+
+  function initButton(button) {
+    setCurrentDot();
+    checkDisabled();
+    currentPosition = newPosition;
+    moveSlides();
+  }
+
+
+
+  if (argument.sliderButtons) {
+    buttonPrev.addEventListener("click", function() {
+      newPosition--;
+      initButton();
+    });
+
+    buttonNext.addEventListener("click", function() {
+      newPosition++;
+      initButton();
+    });
+  }
+
+  if (argument.sliderDots) {
+    makeDots(slidesCount);
+  }
+  // autoMoving();
+  checkDisabled();
 }
 
-let slidera = new slider();
+let sliderConfig = {
+  slider: ".slider",
+  firstPosition: 0,
+  sliderDots: true,
+  sliderButtons: true
+}
 
+let slidera = new slider(sliderConfig);
